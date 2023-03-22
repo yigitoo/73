2d63c5ada05413dcbfbb0bb2c5131ccb5200fd3d
Yiğit GÜMÜŞ <rawns0909@gmail.com>
2023-03-22 20:16:42 +0300

    initpush

---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+
+		to your new<br />SvelteKit app
+	</h1>
+
+	<h2>
+		try editing <strong>src/routes/+page.svelte</strong>
+	</h2>
+
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+</style>
